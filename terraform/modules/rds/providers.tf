89f558516e1e5c29a8520e8271610d38b35c20d4
Joseph Martin <josephmartinrga@gmail.com>
2025-06-03 06:20:47 +0300

    rds: Update tf version

--- terraform/modules/rds/providers.tf
+++ terraform/modules/rds/providers.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.70"
+      version = "~> 5.10.0"
     }
   }
-
-  required_version = ">= 1.0.0"
 }
 
 provider "aws" {
