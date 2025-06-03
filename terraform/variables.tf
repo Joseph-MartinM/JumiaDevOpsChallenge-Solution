957ba349791691da02d0db7e5d5e598439c75002
Joseph Martin <josephmartinrga@gmail.com>
2025-06-03 05:16:38 +0300

    terraform: Update variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -41,8 +41,54 @@ variable "port" {
   default     = "5432"
 }
 
+variable "sshport" {
+  description = "The port that accepts ssh connections"
+  type        = string
+  default     = "1337"
+}
+
+variable "httpsport" {
+  description = "The port that  accepts https connections"
+  type        = string
+  default     = "443"
+}
+
+variable "httpport" {
+  description = "The port that  accepts https connections"
+  type        = string
+  default     = "80"
+}
+
+variable "internetport" {
+  description = "The port that  accepts all connections"
+  type        = string
+  default     = "0"
+}
+
+variable "customlb1port" {
+  description = "The port that  accepts custom lb 8080 connections"
+  type        = string
+  default     = "8080"
+}
+
+variable "customlb2port" {
+  description = "The port that  accepts custom lb 8081 connections"
+  type        = string
+  default     = "8081"
+}
+
 variable "kms_key_id" {
   description = "The ARN for the KMS encryption key. If creating an encrypted replica, set this to the destination KMS ARN. If storage_encrypted is set to true and kms_key_id is not specified the default KMS key created in your account will be used"
   type        = string
   default     = "jumiaKey"
+}
+
+#User Data location
+variable "user_data_location" {
+  default = "/home/ec2-user/rds/modules/rds/ssh_conf.sh"
+}
+
+#Public Key location
+variable "public_key_location" {
+  default = "/home/ec2-user/.ssh/my-key-pair.pub"
 }
